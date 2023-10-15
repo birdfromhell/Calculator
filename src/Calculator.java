a6b4a3e970ff9d33810fdb2a6ffe118292a4e111
Ababil Mustax <95958484+birdfromhell@users.noreply.github.com>
2023-10-15 16:20:22 +0700

    Refactor Calculator class and improve functionality
    Made significant updates to the Calculator application. Refactored the Calculator class to improve the UI by adjusting the JFrame size and JPanel layout. Additionally, improved the logic of the calculator operations. Added functionality for the "CE" (Clear Entry) and "C" (Clear) buttons, allowing the user to clear the current input or clear the entire operation in the calculator. The mathematical operation logic has been moved to a separate calculate() method for better structure and readability of the code. These changes provide the user with a better and more intuitive interface and experience when using the calculator.

--- src/Calculator.java
+++ src/Calculator.java
@@ -3,64 +3,92 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Calculator {
     private JTextField display;
-    private String operator = "";
-    private double firstOperand = 0.0;
-    private boolean isNewEntry = false;
+    private String operator;
+    private double firstOperand;
+    private boolean isNewEntry;
 
     public Calculator() {
-
         JFrame frame = new JFrame("Calculator");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(300,400);
+        frame.setSize(400, 500);
 
-        JPanel panel = new JPanel(new GridLayout(4,5));
+        JPanel panel = new JPanel(new GridLayout(5, 4));
 
-        for (int i = 1; i <= 9; i++){
-            panel.add(new JButton(String.valueOf(i)));
+        display = new JTextField();
+        frame.add(display, BorderLayout.NORTH);
+
+        for (int i = 1; i <= 9; i++) {
+            addButton(panel, String.valueOf(i));
         }
 
-        panel.add(new JButton("*"));
-        panel.add(new JButton("0"));
-        panel.add(new JButton("#"));
-        panel.add(new JButton("+"));
-        panel.add(new JButton("+"));
-        panel.add(new JButton("-"));
-        panel.add(new JButton("/"));
-        panel.add(new JButton("="));
+        addButton(panel, "0");
+        addButton(panel, "+");
+        addButton(panel, "-");
+        addButton(panel, "*");
+        addButton(panel, "/");
+        addButton(panel, "=");
+        addButton(panel, "C");
+        addButton(panel, "CE");
+
         frame.add(panel);
 
         frame.setVisible(true);
     }
 
-    private void addButton(Container container, String text){
+    private void addButton(Container container, String text) {
         JButton button = new JButton(text);
-        button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (text.matches("\\d")) {
-                    if (isNewEntry) {
-                        display.setText("");
-                        isNewEntry = false;
-                    }
-                    display.setText(display.getText() + text);
-                }else {
-                    operator = text;
-                    firstOperand = Double.parseDouble(display.getText());
-                    isNewEntry = true;
-                    if (operator.equals("=")){
-                        display.setText(String.valueOf(firstOperand));
-                        operator = "";
-                    }
+        button.addActionListener(e -> {
+            if (text.matches("\\d")) {
+                if (isNewEntry) {
+                    display.setText("");
+                    isNewEntry = false;
                 }
+                display.setText(display.getText() + text);
+            } else if (text.equals("C")) {
+                firstOperand = 0.0;
+                operator = "";
+                display.setText("");
+            } else if (text.equals("CE")) {
+                display.setText("");
+            } else {
+                if (!operator.isEmpty()) {
+                    calculate();
+                }
+                operator = text;
+                firstOperand = Double.parseDouble(display.getText());
+                isNewEntry = true;
             }
         });
         container.add(button);
     }
-    public static void main(String[] args) {
-        new Calculator();}
 
+    public void calculate() {
+        double secondOperand = Double.parseDouble(display.getText());
+
+        switch (operator) {
+            case "+":
+                firstOperand += secondOperand;
+                break;
+            case "-":
+                firstOperand -= secondOperand;
+                break;
+            case "*":
+                firstOperand *= secondOperand;
+                break;
+            case "/":
+                if (secondOperand != 0) {
+                    firstOperand /= secondOperand;
+                } else {
+                    JOptionPane.showMessageDialog(null, "Cannot divide by zero", "Error", JOptionPane.ERROR_MESSAGE);
+                }
+                break;
+        }
+        display.setText(String.valueOf(firstOperand));
+    }
+
+    public static void main(String[] args) {
+        new Calculator();
+    }
 }
