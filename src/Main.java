13f81a9bb41d24347efcc0de4cc5b830072df000
Ababil Mustax <95958484+birdfromhell@users.noreply.github.com>
2023-10-15 16:01:35 +0700

    Add calculator UI and project configuration files
    The source files for the project were configured, including modules.xml, .gitignore and misc.xml. Specifically, Calculator.iml was set in the modules.xml and necessary files were added in the .gitignore. For the 'Calculator' project, UI components were also added in the Main class, specifically creating a 4x5 grid layout for calculator buttons. Lastly, the vcs.xml file was updated to track the project with Git.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
@@ -8,5 +9,23 @@ public class Main {
         JFrame frame = new JFrame("Calculator");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(300,400);
+
+        JPanel panel = new JPanel(new GridLayout(4,5));
+
+        for (int i = 1; i <= 9; i++){
+            panel.add(new JButton(String.valueOf(i)));
+        }
+
+        panel.add(new JButton("*"));
+        panel.add(new JButton("0"));
+        panel.add(new JButton("#"));
+        panel.add(new JButton("+"));
+        panel.add(new JButton("+"));
+        panel.add(new JButton("-"));
+        panel.add(new JButton("/"));
+        panel.add(new JButton("="));
+        frame.add(panel);
+
+        frame.setVisible(true);
     }
 }
